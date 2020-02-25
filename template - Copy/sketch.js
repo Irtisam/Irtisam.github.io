220de00764d4ac9a0ad2afc332fab3dd54006cac
Irtisam <45469437+Irtisam@users.noreply.github.com>
2020-02-25 15:19:47 -0600

    made some progress...

--- template - Copy/sketch.js
+++ template - Copy/sketch.js
@@ -5,6 +5,7 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let scalar = 1.0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,4 +13,20 @@ function setup() {
 
 function draw() {
   background(220);
+  drawRect()
+}
+
+function drawRect() {
+  fill("red");
+  rect(mouseX-10, mouseY-10, scalar*100, scalar*100,);
+}
+
+function mouseWheel(event) {
+  if (event.delta < 0) {
+    scalar *= 1.1;
+
+  }
+  else {
+    scalar *= 0.9;
+  }
 }
