2f02e357bf502e0a1ef358b929ab728b0de835ba
Irtisam <45469437+Irtisam@users.noreply.github.com>
2020-02-28 14:53:55 -0600

    rotation and color change

--- Interactive scene/sketch.js
+++ Interactive scene/sketch.js
@@ -9,28 +9,37 @@ let scalar = 1.0;
 let angle = 0;
 let bgColor = 0;
 let rectColor = 0;
+let rot = 0;
+rotspeedup = 1;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   bgColor = random(255);
   rectColor = random(255);
+  rectMode(CENTER);
 }
 
 
 function draw() {
-  background(220);
+  background(bgColor);
   drawRect()
 }
 
 function drawRect() {
   noStroke();
+  rot += 0.05;
+  translate(mouseX, mouseY);
+  rotate(rot);
   fill(rectColor);
-  rect(mouseX, mouseY, scalar*100, scalar*100,);
-  if (keyIsPressed) {
+  rect(0, 0, scalar*100, scalar*100,);
+  
+ 
+  if (keyIsPressed && key != "spacebar") {
     rectColor = color(random(255), random(255), random(255));
+    bgColor = color(random(255), random(255), random(255));
   }
 }
 
-function keyPressed()
+
 
 function mouseWheel(event) {
   if (event.delta < 0) {
