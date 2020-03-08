ce41de5633b65982bd60f560ab84b71369b3273a
Irtisam <45469437+Irtisam@users.noreply.github.com>
2020-03-08 14:46:25 -0600

    comments

--- Interactive scene/sketch.js
+++ Interactive scene/sketch.js
@@ -2,9 +2,11 @@
 // Irtisam Islam
 // March 9th, 2020
 //
-// Extra for Experts: Utilization of the mouse wheel
-// - describe what you did to take this project "above and beyond"
+// Extra for Experts:
+// Utilization of the mouse wheel to change the size of the square
+// Use of rotate and translate to continuously rotate the square where the user's cursor is.
 
+// variables used to control the properties of the square and background such as color, size and rotation speed. 
 let scalar = 1.0;
 let angle = 0;
 let bgColor = 0;
@@ -16,17 +18,22 @@ let rotSpeedDown = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
+  // Chooses the background and square color to a random shade white, grey or black. 
   bgColor = random(255);
   rectColor = random(255);
+  
+  // Draws the rectangle from its center, rather than from a corner. 
   rectMode(CENTER);
 }
 
-
+// Sets the random background color and calls the drawRect() function. 
 function draw() {
   background(bgColor);
   drawRect()
 }
 
+// Changes the global boolean variables that control the square's rotation speed and color to true when a specific key is pressed
 function keyPressed() {
   if (key === "w") {
     changeColor = true;
@@ -39,6 +46,7 @@ function keyPressed() {
   }
 }
 
+// Changes global boolean variables back to their original state of false when their corresponding keys are released. 
 function keyReleased() {
   if (key === 'w') {
     changeColor = false;
@@ -51,15 +59,22 @@ function keyReleased() {
   }
 }
 
+// Function that creates the initial rotating square, and also changes its properties when the boolean variables become true. 
 function drawRect() {
   noStroke();
+  
+  // inital rotation speed of square, translates on coordinates of mouse. 
   rot += 0.04;
   translate(mouseX, mouseY);
   rotate(rot);
+  
+  // fills the square color with the variable rectColor which randomly chooses 3 RGB values when it is true. 
   fill(rectColor);
+  
+  // draws the square and changes its size depending on the value of the variable scalar.
   rect(0, 0, scalar*100, scalar*100);
   
- 
+  // Changes the square's color and rotation speed when the conditions are true. 
   if (changeColor) {
     rectColor = color(random(255), random(255), random(255));
     bgColor = color(random(255), random(255), random(255));
@@ -72,8 +87,7 @@ function drawRect() {
   }
 }
 
-
-
+// Function that changes the value of scalar when the mouse wheel is scrolled, which is used in determining the size of the square. 
 function mouseWheel(event) {
   if (event.delta < 0) {
     scalar *= 1.1;
