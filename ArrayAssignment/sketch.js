9c2b0da354088442049fb7cccbf206d27a8f6e77
Irtisam <45469437+Irtisam@users.noreply.github.com>
2020-04-10 14:51:37 -0600

    some index changes

---
+++ ArrayAssignment/sketch.js
@@ -0,0 +1,15 @@
+// Array Assignment - Pong
+// Irtisam Islam
+// April 20, 2020
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
