9b2c63b9080fd48c3346e70e5f17087cbf26f6f4
Irtisam <45469437+Irtisam@users.noreply.github.com>
2020-04-17 18:04:52 -0600

    YAY

--- arrayassignment2/cpu.js
+++ arrayassignment2/cpu.js
@@ -6,7 +6,7 @@ function cpuTurn() {
     for (let j = 0; j < 3; j++) {
       if (board[i][j] === '') {
         board[i][j] = cpu;
-        let score = minimax(board, 0);
+        let score = minimax(board, 0, false);
         board[i][j] = '';
         if (score > bestScore) {
           bestScore = score;
@@ -19,6 +19,44 @@ function cpuTurn() {
   currentPlayer = human;
 }
 
+let scores = {
+  X : 1,
+  O : -1,
+  tie : 0
+}
+
 function minimax(board, depth, maximizing) {
-  return 1;
+  let result = checkWin();
+  if (result !== null) {
+    return scores[result];
+  }
+
+  if (maximizing) {
+    let bestScore = -Infinity;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[i][j] === '') {
+          board[i][j] = cpu;
+          let score = minimax(board, depth +1 , false);
+          board[i][j] = '';
+          bestScore = max(score, bestScore);
+        }
+      }
+    }
+    return bestScore;
+  }
+  else {
+    let bestScore = Infinity;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[i][j] === '') {
+          board[i][j] = human;
+          let score = minimax(board, depth + 1, true);
+          board[i][j] = '';
+          bestScore = min(score, bestScore); 
+        }
+      }
+    }
+    return bestScore;
+  }
 }
