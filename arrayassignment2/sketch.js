4dcdca2d95749835cd2cc8b6b90b816b43f751ba
Irtisam <irtisami@gmail.com>
2022-09-22 21:46:08 -0600

    Update sketch.js
    added cpu function

--- arrayassignment2/sketch.js
+++ arrayassignment2/sketch.js
@@ -1,15 +1,3 @@
-// Tic Tac Toe game with AI
-// Irtisam Islam
-// April 20, 2020
-//
-// Extra for Experts:
-// I incorporated the use of basic AI where the computer player chooses the best move based on the human player's
-// move. This is done by using the minimax algorithm. This algorithm works like a flow chart and assigns values of -1, +1, or 0 to each action the player or CPU makes. The flow chart's 
-// depth starts from one value and then the CPU chooses the largest value that branches off from the inital value making it the maximizing player. the minimizing value is determined
-// by the player who will most likely make a move that is in favor of them. Each time a value is chosen by CPU or player, it branches off into multiple new outcomes and depending on 
-// who's turn it is, the maximizing or minizing value is chosen. The state variable that is used is the currentPlayer variable. Each time either player makes a move the state of 
-//  the currentPlayer variable either changes to human, or cpu. 
-
 // An array of arrays that designates a spot for each square on the board
 let board = [
   ['', '', ''],
@@ -154,5 +142,70 @@ function mousePressed() {
       currentPlayer = cpu;
       cpuTurn();
     }
+  }
+}
+
+
+function cpuTurn() {
+  // AI move
+  let bestScore = -Infinity;
+  let move;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (board[i][j] === '') {
+        board[i][j] = cpu;
+        let score = minimax(board, 0, false);
+        board[i][j] = '';
+        if (score > bestScore) {
+          bestScore = score;
+          move = {i, j};
+        }
+      }
+    }
+  }
+  board[move.i][move.j] = cpu;
+  currentPlayer = human;
+}
+let scores = {
+  X : 1,
+  O : -1,
+  tie : 0
+}
+
+// Function that determines the best move for the CPU by assigning a score to each action the player makes the player makes
+// -1 is the worst scenario for the CPU, +1 is the best, and 0 is a tie. Always attempts for the maximizing value or tries to prevent the minimizing value. 
+function minimax(board, depth, maximizing) {
+  let result = checkWin();
+  if (result !== null) {
+    return scores[result];
+  }
+
+  if (maximizing) {
+    let bestScore = -Infinity;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[i][j] === '') {
+          board[i][j] = cpu;
+          let score = minimax(board, depth + 1 , false);
+          board[i][j] = '';
+          bestScore = max(score, bestScore);
+        }
+      }
+    }
+    return bestScore;
+  }
+  else {
+    let bestScore = Infinity;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[i][j] === '') {
+          board[i][j] = human;
+          let score = minimax(board, depth + 1, true);
+          board[i][j] = '';
+          bestScore = min(score, bestScore); 
+        }
+      }
+    }
+    return bestScore;
   }
 }
